d8ec639e95d9fadac44f7d13a360fc608967d486
JesusGonzalez82 <jgblanco82@yahoo.es>
2025-06-19 18:50:11 +0200

    Primer commit

---
+++ src/main/java/cursispringboot/controllers/GreetingRestController.java
@@ -0,0 +1,16 @@
+package cursispringboot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingRestController {
+
+    @GetMapping("/saludo/{name}")
+    public String greeting(@PathVariable String name){
+        System.out.println("Parece que funciona");
+        return "Hola " + name;
+    }
+
+}
